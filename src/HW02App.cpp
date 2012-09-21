e4c19d8bf85885ec89c72970b5dc9238996a0702
Heather H <hornehm@muohio.edu>
2012-09-20 23:09:37 -0400

    Linked List
    For whatever reason my circle and node class were giving me trouble, so
    I ended up placing them in just one cpp. I am going to try to fix this
    at a later date.

--- src/HW02App.cpp
+++ src/HW02App.cpp
@@ -1,28 +1,55 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
-#include "Circle.h"
-#include "Node.h"
+//#include "Node.h"
+//#include "Circle.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
-class HW02App : public AppBasic {
+class Circle{
+public:
+
+	Circle(int x1, int y1, int r);
+	void draw();
+
+private:
+	int x, y;
+	int radius;
+
+};
+
+Circle::Circle(int x1, int y1, int r){
+	x = x1;
+	y =y1;
+	radius = r;
+}
+
+class Node{
+public:
+	Node* prev;
+	Node* next;
+	Circle* data;
+
+};
+
+
+class HW02App:public AppBasic {
   public:
 	void setup();
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
-
+	
 private:
-	Node* last;
-	Node* start;
 	static const int appWidth = 800;
 	static const int appHeight = 600;
-	Circle* c1;
-	Circle* c2;
+	Node* sentinel;
+	void addToList(Node* sent, Circle* c);
 };
+
+
 
 void HW02App::prepareSettings(Settings* settings){
 	settings->setWindowSize(appWidth, appHeight);
@@ -31,38 +58,50 @@ void HW02App::prepareSettings(Settings* settings){
 void HW02App::setup()
 {
 	//Make Circles!
-	c1 = new Circle(Vec2f(50.0, 50.0), 10);
-	c2 = new Circle(Vec2f(100.0, 100.0), 10);
+	Circle* c1 = new Circle(100, 100, 10);
+	Circle* c2 = new Circle(50, 50, 10);
 
 	//Node list
-	start = NULL;
-	last = NULL;
-
-	for(int i = 0; i < 2; i++){
-		start = new Node(start*, c1);
-		start -> next = start;
+	sentinel = new Node();
+	sentinel -> next = sentinel;
+	sentinel -> prev = sentinel;
+	addToList(sentinel, c1);
+	addToList(sentinel, c2);
+	
+}
+void HW02App::addToList(Node* sent, Circle* c){
+	Node* n = new Node();
+	n -> data = c;
+	n -> next = sent->next;
+	n ->prev = sent;
+	sentinel->next->prev = n;
+	sentinel->next = n;	
 		
-		  
-	}
-
 }
 
 void HW02App::mouseDown( MouseEvent event )
 {
 	if(event.isLeft()){
-
+		event.getX();
+		event.getY();
 	}
 }
 
+
+
 void HW02App::update()
 {
-
+	
 }
 
 void HW02App::draw()
 {
-	
 
+	
 }
 
+
+
 CINDER_APP_BASIC( HW02App, RendererGl )
+
+
